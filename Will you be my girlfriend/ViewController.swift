46ec86c71c49078bf9f1ae457d208f473e26e419
Magzhan Zhumaly <magzhanzhumaly@gmail.com>
2024-01-23 14:50:01 +0600

    Initial Commit

---
+++ Will you be my girlfriend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Will you be my girlfriend
+//
+//  Created by Magzhan Zhumaly on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Will you be my girlfriendUITests/Will_you_be_my_girlfriendUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Will_you_be_my_girlfriendUITestsLaunchTests.swift
+//  Will you be my girlfriendUITests
+//
+//  Created by Magzhan Zhumaly on 23.01.2024.
+//
+
+import XCTest
+
+final class Will_you_be_my_girlfriendUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
